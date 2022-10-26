b34bd4efe84de598a10003955ed94fd326b79558
yutingwu <ding25025@gmail.com>
2022-10-26 22:51:58 +0800

    fix uploadData function

--- uploadData.js
+++ uploadData.js
@@ -1,8 +1,7 @@
 
-function uploadData()
-{
+function uploadData() {
     var getResource = document.getElementById("resourceType").value;
     var getData = JSON.parse(document.getElementById("uploadData").value);
-    var result=document.getElementById("txt_result");
-    PostData(getResource,getData,result);
+    // var result=document.getElementById("txt_result");
+    PostData(getResource, getData);
 }
