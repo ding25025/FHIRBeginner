6fdcb3c55eee64ce51005553ecf7dcb8661bc447
yutingwu <ding25025@gmail.com>
2024-01-09 17:19:16 +0800

    fix(README.md): add REDME content

---
+++ README.md
@@ -0,0 +1,7 @@
+# FHIR 上傳範例程式
+## 檔案說明如下
+- Example:相關範例JSON檔
+- config.js:FHIR 伺服器設定位置、上傳功能
+- Index.html：網頁介面
+- uploadData.js:從網頁介面取得JSON資源，並呼叫上傳功能(練習一)
+- uploadPatient.js:從網頁介面組成病人JSON格式，並呼叫上傳功能(練習二)
--- config.js
+++ config.js
@@ -1,30 +1,28 @@
-const baseUrl = 'http://hapi.fhir.org/baseR4'; //http://hapi.fhir.org/baseR4  https://hapi.fhir.tw/fhir
-const baseheader=new Headers({
-        'Accept': 'application/json',
-        'Content-Type': 'application/fhir+json',
-        
-    });
+const baseUrl = 'http://hapi.fhir.org/baseR4'; //you can change different fhir server url
+//http://hapi.fhir.org/baseR4  https://hapi.fhir.tw/fhir
+const baseheader = new Headers({
+    'Accept': 'application/json',
+    'Content-Type': 'application/fhir+json',
+});
 
-function PostData(resource,jsondata)
-{
+function PostData(resource, jsondata) {
     // input json
     alert(JSON.stringify(jsondata));
-    fetch(`${baseUrl}/`+resource, {
+    fetch(`${baseUrl}/` + resource, {
         method: 'POST',
         body: JSON.stringify(jsondata),
         headers: baseheader
     })
-    .then(response => response.json())
-    .then(function(json) {
-        const area = document.getElementById('resultData');
-        area.textContent = JSON.stringify(json);
-    });
+        .then(response => response.json())
+        .then(function (json) {
+            const area = document.getElementById('resultData');
+            area.textContent = JSON.stringify(json);
+        });
 }
 
-function GetFHIRURL()
-{
-    const fhirsever= document.getElementById('getFHIRURL');
-    fhirsever.textContent=baseUrl;
+function GetFHIRURL() {
+    const fhirsever = document.getElementById('getFHIRURL');
+    fhirsever.textContent = baseUrl;
 }
 
 
